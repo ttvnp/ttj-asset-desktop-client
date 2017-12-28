e10091dd9b2c2cbdb299c3473683e34ed17f9b43
KumanoTanaka <hiroki.tanaka@kumano-te.com>
2017-12-28 16:04:41 +0900

    modify renew recaptcha & server rsa key.

--- src/renderer/config/index.js
+++ src/renderer/config/index.js
@@ -9,12 +9,12 @@ const commonConfig = {
     { name: 'SNP' }
   ],
   dbFilePath: path.join(remote.app.getPath('userData'), '/ttj-desktop-client-database'),
-  recaptchaApiSiteKey: '6LeXSDwUAAAAANKknJNfFqYSeffUl3UnCj-d5vAT'
+  recaptchaApiSiteKey: '6LdtkT4UAAAAAECfIeqlHPNMRuUtz02gjopKBxqG'
 }
 
 const debugConfig = {
   api: {
-    url: 'http://localhost:1324',
+    url: 'http://api-testnet.sencoin.com', // http://localhost:1324,
     rootPath: 'api',
     version: 'v1'
   },
@@ -31,18 +31,18 @@ const debugConfig = {
 
 const releaseConfig = {
   api: {
-    url: 'http://testnet.vnjcoin.com',
+    url: 'https://api.sencoin.com',
     rootPath: 'api',
     version: 'v1'
   },
   serverPubKey: '-----BEGIN PUBLIC KEY-----\n' +
-                'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAo/PSAOtoTNJZvBQEZj6Y\n' +
-                'CGMMrUTCoJGl1A5G3peDX6RxL6Bv8/A6jLvgEvbmpiVQOoAjq4IqIWJHi1YjLeTn\n' +
-                'nIEBmBlKzKkv8zAh11LlDuMEW9QLW5MCCwxClJaG6kTjn5uzc5bDq+3xIPmiunpz\n' +
-                'A52lJPZD8q8m9NYFotFEy0GoGyrwMucvwEzKw2kxW72ZymKU+DRaejc8oD8BTLL+\n' +
-                'nsc9EXajZrVqcpb2gCx0tIzfCA+HlQq0abSuVchpEVWmA2Gt2Veej2RcEk/jJqUy\n' +
-                'y3qbFBUTA/r5jOPX8E0aWQHAkjIreoxa5jmrWEFPeKySD3uPDy7vM35qNNTFR4QN\n' +
-                'BQIDAQAB\n' +
+                'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2RHR14zkJbNTloI1YD9c\n' +
+                'xqRFdmK2h5dw84fD+21Q1GYzlX+BltmsGWrn+rRmwqzuCMJiaObc+93vkXM9pUPU\n' +
+                '1kuwnrhBRCD+zHFU9TYnOmL1TaD9fEp/leH1kHmbzO/HuP9KsSq8k6cjsUGSub3L\n' +
+                'Jz890flOhBsArfmzOJx9O6AWgfZp5JKU8ylqztdSGqGZBg8JQjNtqcNYeI1sOUWh\n' +
+                'pIa//W7L0s0BfbilxtsF/+H77FSw1g9kvwi/7Ulx9VPLmh+NQ00a4sZ36IDhSm5V\n' +
+                'fcZT000CdrEd+I77HRgW1ADQeRUaxtEYf1YDRR8E5hrYs1EbZmUyvAOa6P7NwwME\n' +
+                'KwIDAQAB\n' +
                 '-----END PUBLIC KEY-----'
 }
 
