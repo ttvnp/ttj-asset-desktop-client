f3c132bdb503b4a88c440c09ccfec62ab0cc4eb0
Mizuho Toyoda <toyoda@mango-byte.com>
2018-01-06 07:10:01 +0700

    Add terms of conditions

---
+++ src/renderer/components/TextViewModal.vue
@@ -0,0 +1,106 @@
+<template>
+  <div>
+    <v-dialog
+      v-model="isShow"
+      fullscreen
+      transition="dialog-bottom-transition"
+      :overlay=false
+      scrollable
+    >
+      <v-card>
+        <v-toolbar style="flex: 0 0 auto;" dark class="primary">
+          <v-btn icon @click.native="isShow = false" dark>
+            <v-icon>close</v-icon>
+          </v-btn>
+          <v-toolbar-title>{{ title }}</v-toolbar-title>
+          <v-spacer></v-spacer>
+        </v-toolbar>
+        <v-card-text v-html="body"></v-card-text>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        isShow: false
+      }
+    },
+    props: ['title', 'body'],
+    methods: {
+      open () {
+        this.isShow = true
+      },
+
+      close () {
+        this.isShow = false
+        this.$emit('modal-close')
+      },
+
+      doNothing () {
+      }
+    },
+
+    components: {
+    }
+  }
+</script>
+
+<style scoped>
+  .modal-mask {
+    position: fixed;
+    z-index: 9998;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, .5);
+    display: table;
+    transition: opacity .3s ease;
+  }
+
+  .modal-wrapper {
+    display: table-cell;
+    vertical-align: middle;
+  }
+
+  .modal-container {
+    width: 300px;
+    margin: 0px auto;
+    padding: 20px 30px;
+    background-color: #fff;
+    border-radius: 2px;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
+    transition: all .3s ease;
+    font-family: Helvetica, Arial, sans-serif;
+  }
+
+  .modal-header h3 {
+    margin-top: 0;
+    color: #42b983;
+  }
+
+  .modal-body {
+    margin: 20px 0;
+  }
+
+  .modal-button {
+    float: right;
+  }
+
+  .modal-enter {
+    opacity: 0;
+  }
+
+  .modal-leave-active {
+    opacity: 0;
+  }
+
+  .modal-enter .modal-container,
+  .modal-leave-active .modal-container {
+    -webkit-transform: scale(1.1);
+    transform: scale(1.1);
+  }
+</style>
